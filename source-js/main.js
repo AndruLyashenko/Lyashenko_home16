512bf2e235ab3422fe0731737279ea85a6e16201
AndruLyashenko <laavllag85@gmail.com>
2017-02-12 05:09:42 +0400

    'scriptfixednext'

--- source-js/main.js
+++ source-js/main.js
@@ -1,13 +1,38 @@
-$('.accordeon-button').on('click',function() {
+$('.accordeon-button').on('click', function () {
     var id = $(this).attr('data-id');
+    $('.accordeon-button').addClass('active');
+    $('.accordeon-button.active').removeClass('active');
+    $('.accordeon-text').addClass('active');
     $('.accordeon-text.active').removeClass('active');
-    $('.accordeon-text[data-id="' + id  + '"]').toggleClass('active');
+    $('.accordeon-text[data-id="' + id + '"]').toggleClass('active');
 });
-$('.tooltip').on('hover', function() {
+
+$('.dialog-button').on('click', function () {
+    var id = $(this).attr('data-id');
+    $('.dialog-blok').addClass('active');
+    $('.dialog-blok-image').on('click', function () {
+        $('.dialog-blok.active').removeClass('active');
+    })
+});
+
+
+$('.tabs-button').on('click', function () {
+    var id = $(this).attr('data-id');
+    $('.tabs-button').addClass('active');
+    $('.tabs-button.active').removeClass('active');
+    $('.tabs-text').addClass('active');
+    $('.tabs-text.active').removeClass('active');
+    $('.tabs-text[data-id="' + id + '"]').toggleClass('active');
+});
+
+//більш-менш ясно,але вистачає питань і не все працює як потрібно
+
+$('.tooltip-blok').on('hover', function () {
     var left = $(this).offsetLeft;
     var top = $(this).offsetTop;
-    $('.tooltip-text').addClass('active').css({
+    $('.tooltip-hover').addClass('active').css({
         left: left + 'px',
         top: top + 30 + 'px'
     })
 });
+//не зрозуміло мені тултіп, треба разом ще раз спробувати
